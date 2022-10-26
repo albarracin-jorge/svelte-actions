29da3d58d5df1214e9ea6a1534826f06b9798a63
albarracin-jorge <albarracinj.dev@gmail.com>
2022-10-26 07:40:55 -0300

    [edit] edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://albarracin-jorge.github.io/svelte-actions',
   plugins: [svelte()]
 })
